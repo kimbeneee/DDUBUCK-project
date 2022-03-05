78a537ca34a69ffdf15ca2424d096ca9168db031
kim <kimbeneeee@gmail.com>
2022-03-05 18:33:14 +0900

    properties add

---
+++ DDUBUCK_common/src/main/java/com/ddubuck/common/config/ApplicationConfig.java
@@ -0,0 +1,10 @@
+package com.ddubuck.common.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@EntityScan({"com.ddubuck.common.model"})
+public class ApplicationConfig {
+}
---
+++ DDUBUCK_web/src/main/java/com/ddubuck/web/WebApplication.java
@@ -0,0 +1,13 @@
+package com.ddubuck.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.ddubuck"})
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
